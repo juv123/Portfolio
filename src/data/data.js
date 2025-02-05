5c6e8a2ec5270df2c2527afb62b57caaedee4f21
juv123 <deegaaug@gmail.com>
2025-02-05 17:52:41 +0530

    added new projects

--- src/data/data.js
+++ src/data/data.js
@@ -7,58 +7,59 @@ import foodie from '../assets/projects/foodie.png'
 import rideme from '../assets/projects/horseride.png'
 import currency_converter from '../assets/projects/currency-converter.png'
 import expense_tracker from '../assets/projects/expensetracker.png'
-import rbac from '../assets/projects/rbac.png'
+import dashboard_fullstack from '../assets/projects/dashboard_fullstack.png'
 export const data=[
-    
+    {
+        id:1,
+        name:"Admin Dashboard",
+        image:dashboard_fullstack,
+        github:"https://github.com/juv123/admin-dashboard-fullstack",
+        live:"https://www.loom.com/share/7b4e4f8e05014d1f88668ef622904856?sid=ccf5d898-2562-4c00-b8b3-c427e8478340",
+    },
     {
         id:2,
+        name:"My Expense Tracker",
+        image:expense_tracker,
+        github:"https://github.com/juv123/expense-tracker-frontend",
+        live:"https://www.loom.com/share/3453222ebdf74055aa47f10c078defde?sid=fdad7174-8f37-4aed-a79e-b289f6b8c70b",
+    },
+    {
+        id:3,
         name:"Netflix GPT",
         image:netflix,
         github:"https://github.com/juv123/Netflix",
         live:"https://www.loom.com/share/e991fefb5b6842cf959d73494ac9961f?sid=3d145542-3515-4835-aac3-e258779cf73f",
     },
     {
-        id:3,
+        id:4,
         name:"Youtube",
         image:youtube,
         github:"https://github.com/juv123/Youtube",
         live:"https://www.loom.com/share/6d8d2f702fe24c5bbb0cfa86b8a1e0c2?sid=50543e85-167d-4deb-a46b-c599cf5e1dea",
     },
     {
-        id:4,
+        id:5,
         name:"Foodie",
         image:foodie,
         github:"https://github.com/juv123/Foodie",
         live:"https://www.loom.com/share/9062b87dc5a4401aad96fa14f8349906?sid=87c14e99-264d-4b5b-b999-2ae1800f77e4",
     },
     {
-        id:5,
+        id:6,
         name:"Ride Me",
         image:rideme,
         github:"https://github.com/juv123/HorseRideBooking",
         live:"https://www.loom.com/share/f0cf384fd4e34caf9eb1a5061956282d?sid=67d5371a-0e73-45b2-9c1e-bfdfe3536350",
     },
     {
-        id:6,
+        id:7,
         name:"Currency Converter",
         image:currency_converter,
         github:"https://github.com/juv123/CurrencyConverter",
         live:"https://currency-converter-r7zv-nsqp8t2ya-juv123s-projects.vercel.app/",
     },
-    {
-        id:7,
-        name:"Admin Dashboard",
-        image:rbac,
-        github:"https://github.com/juv123/RBAC",
-        live:"https://www.loom.com/share/02d58a29294d46fcaed36061d8e98d5d?sid=63d2c988-005e-461d-bc0a-0687b0a58563",
-    },
-    {
-        id:8,
-        name:"My Expense Tracker",
-        image:expense_tracker,
-        github:"https://github.com/juv123/expense-tracker-frontend",
-        live:"https://www.loom.https://www.loom.com/share/db9fb3ebf3f6482c8ee6d1892d90bad0?sid=35b94563-0d93-4288-b096-67b01321f75d/share/4a9bc79dc7c14c08a6cf542bc71af8e7",
-    },
+   
+   
 
 
 ]
